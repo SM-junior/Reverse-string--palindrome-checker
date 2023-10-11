33283650164c9e100ca5688314a2e946d822d407
SM-junior <shahinalam12341@gmail.com>
2023-10-12 00:12:53 +0600

    done

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,16 @@
-// function palindrome(str){
-//     const reversed=str.split('').reverse().join('');
-//     if(str===reversed){
-//         console.log(true);
-//     }
-//     else{
-//         console.log(false);
-//     }
-// }
-// palindrome('shahin')
+// method-1
+function palindrome(str){
+    const reversed=str.split('').reverse().join('');
+    if(str===reversed){
+        console.log(true);
+    }
+    else{
+        console.log(false);
+    }
+}
+palindrome('shahin')
 
+//method-2
 function palindrome(str){
     const reversed=str.split('').reverse().join('');
     return str===reversed
--- reverseString.js
+++ reverseString.js
@@ -1,9 +1,12 @@
-// function reverseString(str){
-//     const reversed=str.split('').reverse().join('');
-//     console.log(reversed);
-// }
-// reverseString('shahin')
+//method-1
+function reverseString(str){
+    const reversed=str.split('').reverse().join('');
+    console.log(reversed);
+}
+reverseString('shahin')
 
+
+//method-2
 function reverseString(str){
     let reversed='';
     for(let char of str){
